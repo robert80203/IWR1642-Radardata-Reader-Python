fda35e4f8755fd0006406299fd8e5e301cf128e0
robert80203 <robert80203@gmail.com>
2021-07-08 20:28:37 +0800

    update process_radarData.py

--- process_radarData.py
+++ process_radarData.py
@@ -7,19 +7,13 @@ from PIL import Image
 import json
 
 class RadarObject():
-    def __init__(self, visualization=True):
-        self.radarDataFileNameGroup = [
-            ['raw_data/iwr1642/20210609/single_1/hori', 'raw_data/iwr1642/20210609/single_1/verti']
-        ]
-        self.saveDirNameGroup =  [
-            'processed_data/iwr1642/20210609/single_1'
-        ]
-        self.rgbFileNameGroup = [
-            'frames/20210609/single_1/processed/images'
-        ]
-        self.jointsFileNameGroup = [
-            'processed_data/iwr1642/20210609/single_1/annot/hrnet_annot.json'
-        ]
+    def __init__(self, visualization=True, saveWithChirp=False):
+        numGroup = 2#30 + 1
+        root = '20210628'
+        self.radarDataFileNameGroup = []
+        self.saveDirNameGroup =  []
+        self.rgbFileNameGroup = []
+        self.jointsFileNameGroup = []
         self.numADCSamples = 128
         self.numADCBits = 1
         self.numRX = 4
@@ -27,14 +21,29 @@ class RadarObject():
         self.framePerSecond = 30
         self.duration = 60
         self.numFrame = self.framePerSecond * self.duration
-        self.numChirp = 128
-        self.numKeypoints = 17
+        self.numChirp = 64 # numChirp//2 represents using BPM setting in DCA1000EVM
+        self.idxProcChirp = 16 # manually decided
+        self.numKeypoints = 16#17
         self.xIndices = [-45, -30, -15, 0, 15, 30, 45]
         self.yIndices = [i * 10 for i in range(10)]
         self.visualization = visualization
+        self.saveWithChirp = saveWithChirp
         self.showPose = True
         self.showHeatmap = True
-    
+
+        self.initialize(root, numGroup)
+
+    def initialize(self, root, numGroup):
+        for i in range(1, numGroup):
+            radarDataFileName = ['raw_data/iwr1642/' + root + '/single_' + str(i) + '/hori', 'raw_data/iwr1642/20210628/single_' + str(i) + '/verti']
+            saveDirName = 'processed_data/iwr1642/' + root + '/single_' + str(i)
+            rgbFileName = 'frames/' + root + '/single_' + str(i) + '/processed/images'
+            jointsFileName = 'processed_data/iwr1642/' + root + '/single_' + str(i) + '/annot/hrnet_annot.json'
+            self.radarDataFileNameGroup.append(radarDataFileName)
+            self.saveDirNameGroup.append(saveDirName)
+            self.rgbFileNameGroup.append(rgbFileName)
+            self.jointsFileNameGroup.append(jointsFileName)
+
     def getadcDataFromDCA1000(self, fileName):
         with open(fileName, 'rb') as fp:
             adcData = np.fromfile(fp, np.int16)
@@ -125,9 +134,16 @@ class RadarObject():
         for idxChirp in range(self.numChirp//2):
             dataRangeAzimuth[:, idxChirp, :] = np.fft.fft2(dataRangeAzimuth[:, idxChirp, :])
         #dataFFT = dataRangeAzimuth[:,:,0:self.numADCSamples].max(axis=1)
-        dataFFT = dataRangeAzimuth[:,0,0:self.numADCSamples//2]
-        dataFFT = self.postProcessFFT(dataFFT)
-        return dataFFT
+        if self.saveWithChirp:
+            dataFFTGroup = np.zeros((self.idxProcChirp, self.numADCSamples//2, self.numRX*2), dtype='complex_')
+            for idxChirp in range(self.idxProcChirp):
+                dataFFT = dataRangeAzimuth[:,idxChirp,0:self.numADCSamples//2]
+                dataFFTGroup[idxChirp, :, :] = self.postProcessFFT(dataFFT)
+            return dataFFTGroup
+        else:
+            dataFFT = dataRangeAzimuth[:,0,0:self.numADCSamples//2]
+            dataFFT = self.postProcessFFT(dataFFT)
+            return dataFFT
 
     def generateRangeDopplerMap(self, frame):
         dataRangeDoppler = np.zeros((self.numRX*2, self.numChirp//2, self.numADCSamples), dtype='complex_')
@@ -155,8 +171,15 @@ class RadarObject():
             idxFrame, mapRangeAzimuthHori, mapRangeAzimuthVert, 
             pose, heatmap)
 
-    def saveRadarData(self, matrix, dirName):
-        np.savetxt(dirName, np.column_stack([matrix.real, matrix.imag]))
+    def saveRadarData(self, matrix, dirName, idxFrame):
+        if self.saveWithChirp:
+            for idxChirp in range(self.idxProcChirp):
+                idxChirpFrame = (idxFrame * (self.idxProcChirp)) + idxChirp
+                dirSave = dirName + ('/%09d' % idxChirpFrame) + '.txt'
+                np.savetxt(dirSave, np.column_stack([matrix[idxChirp].real, matrix[idxChirp].imag]))
+        else:
+            dirSave = dirName + ('/%09d' % idxFrame) + '.txt'
+            np.savetxt(dirSave, np.column_stack([matrix.real, matrix.imag]))
 
     def processRadarData(self):
         for idxName in range(len(self.radarDataFileNameGroup)):
@@ -172,8 +195,10 @@ class RadarObject():
                 frameVert = adcDataVert[:, self.numChirp*(idxFrame):self.numChirp*(idxFrame+1), 0:self.numADCSamples]
                 mapRangeAzimuthHori = self.generateRangeAzimuthMap(frameHori)
                 mapRangeAzimuthVert = self.generateRangeAzimuthMap(frameVert)
-                
                 if self.visualization:
+                    if self.saveWithChirp:
+                        mapRangeAzimuthHori = mapRangeAzimuthHori[0,:,:]#only take the first chirp
+                        mapRangeAzimuthVert = mapRangeAzimuthVert[0,:,:]
                     visDirName = self.saveDirNameGroup[idxName] + '/visualization' + ('/%09d.png' % idxFrame)
                     img = np.array(Image.open(self.rgbFileNameGroup[idxName] + "/%09d.jpg" % idxFrame).convert('RGB'))
                     joints = annotGroup[idxFrame]['joints']
@@ -181,13 +206,12 @@ class RadarObject():
                         mapRangeAzimuthHori, mapRangeAzimuthVert, 
                         visDirName, idxFrame)
                 else:
-                    horiDirName = self.saveDirNameGroup[idxName] + '/hori' + ('/%09d' % idxFrame) + '.txt'
-                    self.saveRadarData(mapRangeAzimuthHori, horiDirName)
-                    vertDirName = self.saveDirNameGroup[idxName] + '/verti' + ('/%09d' % idxFrame) + '.txt'
-                    self.saveRadarData(mapRangeAzimuthVert, vertDirName)
+                    horiDirName = self.saveDirNameGroup[idxName] + '/hori' #+ ('/%09d' % idxFrame) + '.txt'
+                    self.saveRadarData(mapRangeAzimuthHori, horiDirName, idxFrame)
+                    vertDirName = self.saveDirNameGroup[idxName] + '/verti' #+ ('/%09d' % idxFrame) + '.txt'
+                    self.saveRadarData(mapRangeAzimuthVert, vertDirName, idxFrame)
 
-                print('Finished frame %d' % idxFrame)
-
+                print('%s, finished frame %d' % (self.radarDataFileNameGroup[idxName][0], idxFrame))
 if __name__ == "__main__":
-    radarObject = RadarObject(visualization=True)
+    radarObject = RadarObject(visualization=True, saveWithChirp=False)
     radarObject.processRadarData()
