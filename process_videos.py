745757809e12eb3df36104840b726b29cff2bffb
robert80203 <robert80203@gmail.com>
2021-07-12 16:12:49 +0800

    update process_videos.py

--- process_videos.py
+++ process_videos.py
@@ -4,14 +4,16 @@ import torch
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 
-def GenerateFramesfromVideo(video_path, save_path):
+def GenerateFramesfromVideo(video_path, save_path, isFliped=False):
     vidcap = cv2.VideoCapture(video_path)
     success, image = vidcap.read()
     count = 0
     while success:
+        if isFliped:
+            image = cv2.flip(image, 0)
         cv2.imwrite(save_path+"/%09d.jpg" % count, image)     # save frame as JPEG file      
         success, image = vidcap.read()
-        print('Save a new frame: %d' % count)
+        print('%s, save a new frame: %d' % (video_path, count))
         count += 1
     return count
 
@@ -22,7 +24,7 @@ def PreprcessFrames(num_frames, load_path, save_path):
         img = Image.open(load_path+"/%09d.jpg" % i).convert('RGB')
         img = transform_fn(img)
         #print(img.size())
-        img = F.interpolate(img.unsqueeze(0), size=[256, 256])
+        img = F.interpolate(img.unsqueeze(0), size=[512, 512]) # 256, 256
         img = toimg(img.squeeze(0))
         img.save(save_path+"/%09d.jpg" % i)
         print('Save a new frame: %d' % i)
@@ -36,10 +38,10 @@ def PreprcessSpecificFrames(load_path, save_path, p_frame_num):
         img = Image.open(load_path+"/%09d.jpg" % idx).convert('RGB')
         img = transform_fn(img)
         #print(img.size())
-        img = F.interpolate(img.unsqueeze(0), size=[256, 256])
+        img = F.interpolate(img.unsqueeze(0), size=[512, 512]) # 256, 256
         img = toimg(img.squeeze(0))
         img.save(save_path+"/%09d.jpg" % i)
-        print('Save a old frame %d as a new frame %d' % (idx, i))
+        print('%s, save a old frame %d as a new frame %d' % (save_path, idx, i))
         i += 1
 
 def GenerateVideofromFrames(frame_path, save_path):
@@ -59,34 +61,74 @@ def GenerateVideofromFrames(frame_path, save_path):
         videoWrite.write(img)
         print(i)
 
+#for 20210609
+#startFrameGroup = [
+#        139,
+#        75,
+#        63,
+#        147,
+#        103,
+#        247
+#    ]
+
+# fpr 20210628
+# startFrameGroup = [
+#         156,
+#         96,
+#         22,
+#         167,
+#         275,
+#         12,
+#         21,
+#         8,
+#         17,
+#         16,
+#         423,
+#         192,
+#         207,
+#         182,
+#         233,
+#         205,
+#         386,
+#         231,
+#         171,
+#         158,
+#         6,
+#         8,# should be 14, but total length is not enough
+#         6,
+#         8,
+#         0,
+#         8,
+#         3,
+#         11,
+#         7,
+#         12
+#     ]
+
+
+#for 20210628
+#the video clips should be flipped (isFliped=True)
 
 if __name__ == "__main__":
-    nameGroup = [
-    'single_1',
-    #'single_2',
-    #'single_3',
-    #'single_4',
-    #'single_5',
-    #'single_6',
-    ]
-    startFrameGroup = [
-        #139,
-        #75,
-        #63,
-        147,
-        #103,
-        #247
-    ]
+    
+    nameGroup = ['single_%d' % i for i in range(1,18)]
+    root = '20210712'
+    
     for idxName in range(len(nameGroup)):
         name = nameGroup[idxName]
-        pathVideo = "videos/20210609/" + name + ".avi"
-        #pathRawFrame = "frames/20210609/" + name + "/raw"
-        #pathProcessedFrame = "frames/20210609/" + name + "/processed/images"
-        
-        #numFrames = GenerateFramesfromVideo(pathVideo, pathRawFrame)
+        pathVideo = "videos/" + root + "/" + name + ".mp4" # ".avi"
+        pathRawFrame = "frames/" + root + "/" + name + "/raw"
+        numFrames = GenerateFramesfromVideo(pathVideo, pathRawFrame, True)
         #PreprcessFrames(numFrames, pathRawFrame, pathProcessedFrame)
-        
-        #PreprcessSpecificFrames(pathRawFrame, pathProcessedFrame, startFrameGroup[idxName])
-        
-        pathVideoFrame = "processed_data/iwr1642/20210609/" + name + "/visualization"
-        GenerateVideofromFrames(pathVideoFrame, pathVideo)
+
+    # startFrameGroup = [
+    # ]
+    # for idxName in range(len(nameGroup)):
+    #     name = nameGroup[idxName]
+    #     pathRawFrame = "frames/" + root + "/" + name + "/raw"
+    #     pathProcessedFrame = "frames/" + root + "/" + name + "/processed/images"
+    #     PreprcessSpecificFrames(pathRawFrame, pathProcessedFrame, startFrameGroup[idxName])
+    
+    # pathVideoFrame = "/media/pc3426/B2E67301E672C4DF/radar_skeleton_estimation/radar_skeleton_estimation_network/visualization/for20210706_presentation/multiFramesChirps_tmv2_20210707_val_2"
+    # pathVideo = "./videos/20210628/multiFramesChirps_tmv2_20210707_val_2.avi"
+    # GenerateVideofromFrames(pathVideoFrame, pathVideo)
