e7b6a9f56d0b576b54fbfc738ec75d9303edea94
826cae6c-94b3-4b71-829a-73dd4605495c <robert80203@gmail.com>
2022-02-09 14:24:29 +0800

    1. update

--- process_radarData.py
+++ process_radarData.py
@@ -5,50 +5,59 @@ import scipy.io
 from plot_utils import PlotMaps, PlotPose, PlotHeatmaps
 from PIL import Image
 import json
+import torch
+import torch.nn.functional as F
 #import mmwave as mm
 #from mmwave.dataloader import DCA1000
 
 class RadarObject():
-    def __init__(self, visualization=True, saveWithChirp=False):
-        numGroup = 1#30 + 1
-        self.root = '20211008'#'20210712'
-        self.saveRoot = '20211008_Pad_CltRm'#'20210712_Pad_CltRm_RDA'
+    def __init__(self, visualization=False):
+        numGroup = 30 + 1
+        self.root = '20211220' #'20210712'
+        self.saveRoot = '20211220_Pad_CltRm_RDA' #'20210712_Pad_CltRm_RDA'
         self.radarDataFileNameGroup = []
         self.saveDirNameGroup =  []
         self.rgbFileNameGroup = []
         self.jointsFileNameGroup = []
-        self.numADCSamples = 256 # 0916: 128, 1008: 256
-        self.numAngleBins = self.numADCSamples//2 # half of range/numADCsamples
+        self.numADCSamples = 256 # 0916: 128, 1008: 256, 1106: 128, 1220: 256, 0107: 128
+        self.adcRatio = 4
+        self.numAngleBins = self.numADCSamples//self.adcRatio # half of range/numADCsamples
         self.numADCBits = 1
         self.numRX = 4
         self.numLanes = 2
         self.framePerSecond = 30
         self.duration = 60
         self.numFrame = self.framePerSecond * self.duration
-        self.numChirp = 128 #0916: 256, 1008: 128, 2 times of the num of chirp you set in TI tool
-        self.idxProcChirp = 64 #0916: 128, 1008: 64 # manually decided how many chirps you want to process
-        self.numGroupChirp = 4 #0916: 8, 1008: 8 # manually decided the chirp duration, e.g.  64/4 = totally has 16 chirp
-        self.numKeypoints = 16#17
+        self.numChirp = 128 #0916: 256, 1008: 128, 1106: 256, 1220: 128, 0107: 256 # 2 times of the num of chirp you set in TI tool
+        self.idxProcChirp = 64 #0916: 128, 1008: 64, 1106: 128, 1220: 64, 0107: 128 # manually decided how many chirps you want to process
+        self.numGroupChirp = 4 #1106:8 1220:8, 0107:8 # manually decided the chirp duration, e.g.  64/4 = totally has 16 chirp
+        self.numKeypoints = 14 
         self.xIndices = [-45, -30, -15, 0, 15, 30, 45]
         self.yIndices = [i * 10 for i in range(10)]
         self.visualization = visualization
-        self.saveWithChirp = saveWithChirp
-        self.showPose = False
-        self.showHeatmap = False
+        #self.saveWithChirp = saveWithChirp
+        self.showPose = False #always set it to false
+        self.showHeatmap = True
+        self.useVelocity = True
+        #self.downscaleRange = True
 
+        #leftVIndex = {i - self.idxProcChirp//2: -i for i in range(self.idxProcChirp//2)}
+        leftVIndex = {i - self.idxProcChirp//2: -(i+1) for i in range(self.idxProcChirp//2)}
+        rightVIndex = {i:self.idxProcChirp//2 - i for i in range(self.idxProcChirp//2)}
+        self.mergeDict = {**leftVIndex, **rightVIndex}
         #self.dca = DCA1000()
         self.initialize(numGroup)
 
     def initialize(self, numGroup):
-        for i in range(1, numGroup + 1):
-        #for i in range(5, 6):
+        #for i in range(1, numGroup + 1):
+        for i in range(16, 31):
             radarDataFileName = ['raw_data/iwr1642/' + self.root + '/single_' + str(i) + '/hori', 
                                  'raw_data/iwr1642/' + self.root + '/single_' + str(i) + '/vert']
-            saveDirName = 'processed_data/iwr1642/' + self.saveRoot + '/single_' + str(i)
-            #saveDirName = 'radar_skeleton_estimation/data/' + self.saveRoot + '/single_' + str(i)
+            #saveDirName = 'processed_data/iwr1642/' + self.saveRoot + '/single_' + str(i)
+            saveDirName = 'radar_skeleton_estimation/data/' + self.saveRoot + '/single_' + str(i)
             rgbFileName = 'frames/' + self.root + '/single_' + str(i) + '/processed/images'
-            jointsFileName = 'processed_data/iwr1642/' + self.saveRoot + '/single_' + str(i) + '/annot/hrnet_annot.json'
-            #jointsFileName = 'radar_skeleton_estimation/data/' + self.saveRoot + '/single_' + str(i) + '/annot/hrnet_annot.json'
+            #jointsFileName = 'processed_data/iwr1642/' + self.saveRoot + '/single_' + str(i) + '/annot/hrnet_annot.json'
+            jointsFileName = 'radar_skeleton_estimation/data/' + self.saveRoot + '/single_' + str(i) + '/annot/hrnet_annot.json'
             self.radarDataFileNameGroup.append(radarDataFileName)
             self.saveDirNameGroup.append(saveDirName)
             self.rgbFileNameGroup.append(rgbFileName)
@@ -221,30 +230,27 @@ class RadarObject():
         for idxChirp in range(self.numChirp//2):
             dataRangeAzimuth[:, idxChirp, :] = np.fft.fft2(dataRangeAzimuth[:, idxChirp, :])
         #dataFFT = dataRangeAzimuth[:,:,0:self.numADCSamples].max(axis=1)
-        if self.saveWithChirp:
-            dataFFTGroup = np.zeros((self.idxProcChirp//self.numGroupChirp, self.numADCSamples//2, self.numAngleBins), dtype='complex_')
+        #if self.saveWithChirp:
+        if not self.visualization:
+            dataFFTGroup = np.zeros((self.idxProcChirp//self.numGroupChirp, self.numADCSamples//self.adcRatio, self.numAngleBins), dtype='complex_')
             #dataFFTGroup = np.zeros((self.idxProcChirp//self.numGroupChirp, self.numADCSamples, self.numAngleBins), dtype='complex_')
             #dataFFTGroup = np.zeros((self.idxProcChirp, self.numADCSamples//2, self.numAngleBins), dtype='complex_')
             i = 0
             for idxChirp in range(0, self.idxProcChirp, self.numGroupChirp):
                 #for idxChirp in range(32, 64+32, 8):
-                dataFFT = dataRangeAzimuth[:,idxChirp,0:self.numADCSamples//2]
+                dataFFT = dataRangeAzimuth[:,idxChirp,0:self.numADCSamples//self.adcRatio]
                 dataFFTGroup[i, :, :] = self.postProcessFFT(dataFFT)
                 i += 1
             return dataFFTGroup
         else:
-            dataFFT = dataRangeAzimuth[:,0,0:self.numADCSamples//2]
+            dataFFT = dataRangeAzimuth[:,0,0:self.numADCSamples//self.adcRatio]
             #dataFFT = dataRangeAzimuth[:,0,0:self.numADCSamples]
             dataFFT = self.postProcessFFT(dataFFT)
             return dataFFT
 
-    def generateRangeAzimuthMapV2(self, frame):
+    def generateRangeDopplerAzimuthMap(self, frame):
         dataRangeAzimuth = np.zeros((self.numRX*2, self.numChirp//2, self.numADCSamples), dtype='complex_')
-        # for idxRX in range(self.numRX):
-        #     for idxChirp in range(self.numChirp//2):
-        #         dataRangeAzimuth[idxRX, idxChirp] = (frame[idxRX, idxChirp*2] + frame[idxRX, idxChirp*2+1])/2
-        #         dataRangeAzimuth[idxRX+4, idxChirp] = (frame[idxRX, idxChirp*2] - frame[idxRX, idxChirp*2+1])/2
-        
+
         # Process radar data with TDM-MIMO
         for idxRX in range(self.numRX):
             for idxChirp in range(self.numChirp):
@@ -254,55 +260,63 @@ class RadarObject():
                     dataRangeAzimuth[idxRX+4, idxChirp//2] = frame[idxRX, idxChirp]
 
         # perform static clutter removal, along with chirps
-        #dataRangeAzimuth = np.transpose(dataRangeAzimuth, (1, 0, 2))
-        #dataRangeAzimuth = self.clutterRemoval(dataRangeAzimuth, axis=0)
-        #dataRangeAzimuth = np.transpose(dataRangeAzimuth, (1, 0, 2))
+        dataRangeAzimuth = np.transpose(dataRangeAzimuth, (1, 0, 2))
+        dataRangeAzimuth = self.clutterRemoval(dataRangeAzimuth, axis=0)
+        dataRangeAzimuth = np.transpose(dataRangeAzimuth, (1, 0, 2))
 
-        # Range FFT
-        for idxRX in range(self.numRX*2):
-            for idxChirp in range(self.numChirp//2):
-                dataRangeAzimuth[idxRX, idxChirp, :] = np.fft.fft(dataRangeAzimuth[idxRX, idxChirp, :])
-        # Doppler FFT
-        for idxRX in range(self.numRX*2):
-            for idxADC in range(self.numADCSamples):
-                dataRangeAzimuth[idxRX, :, idxADC] = np.fft.fft(dataRangeAzimuth[idxRX, :, idxADC])
+        for idxRX in range(self.numRX * 2):
+            dataRangeAzimuth[idxRX, :, :] = np.fft.fft2(dataRangeAzimuth[idxRX, :, :])
+
         padding = ((0, self.numAngleBins - dataRangeAzimuth.shape[0]), (0,0), (0,0))
+        #padding = ((0, 512 - 8), (0,0), (0,512 - 128))
         dataRangeAzimuth = np.pad(dataRangeAzimuth, padding, mode='constant')
-        # Azimuth FFT
+
         for idxChirp in range(self.numChirp//2):
             for idxADC in range(self.numADCSamples):
                 dataRangeAzimuth[:, idxChirp, idxADC] = np.fft.fft(dataRangeAzimuth[:, idxChirp, idxADC])
-        #dataFFT = dataRangeAzimuth[:,:,0:self.numADCSamples].max(axis=1)
-        if self.saveWithChirp:
-            pass
-            # dataFFTGroup = np.zeros((self.idxProcChirp, self.numADCSamples//2, self.numAngleBins), dtype='complex_')
-            # for idxChirp in range(self.idxProcChirp):
-            #     dataFFT = dataRangeAzimuth[:,idxChirp,0:self.numADCSamples//2]
-            #     dataFFTGroup[idxChirp, :, :] = self.postProcessFFT(dataFFT)
-            # return dataFFTGroup
-        else:
-            #dataFFT = dataRangeAzimuth[:,0,0:self.numADCSamples//2]
-            dataFFT = np.mean(dataRangeAzimuth[:, :, 0:self.numADCSamples//2], 1)
-            dataFFT = self.postProcessFFT(dataFFT)
+        
+        #specific range
+        idxADCSpecific = [i for i in range(76, 140)]
+        
+        if not self.visualization:
+            # change velocity index
+            #-----------------------------
+            #| q | 0 |  |  |  |  | 0 | k |
+            #-----------------------------
+            #             ||
+            #             \/
+            #-----------------------------
+            #|  |  | 0 | q | k | 0 |  |  |
+            #-----------------------------
+            dataTemp = np.zeros((self.idxProcChirp, self.numADCSamples//self.adcRatio, self.numAngleBins), dtype='complex_')
+            dataFFTGroup = np.zeros((self.idxProcChirp//self.numGroupChirp, self.numADCSamples//self.adcRatio, self.numAngleBins), dtype='complex_')
+            for idxRX in range(self.numAngleBins):
+                for idxADC in range(self.numADCSamples//self.adcRatio):
+                    #dataTemp[:, idxADC, idxRX] = dataRangeAzimuth[idxRX, :, idxADC]
+                    #dataTemp[:, idxADC, idxRX] = np.fft.fftshift(dataTemp[:, idxADC, idxRX], axes=(0))
+                    dataTemp[:, idxADC, idxRX] = dataRangeAzimuth[idxRX, :, idxADCSpecific[idxADC]]
+                    dataTemp[:, idxADC, idxRX] = np.fft.fftshift(dataTemp[:, idxADC, idxRX], axes=(0))
+            chirpPad = self.idxProcChirp//self.numGroupChirp
+            i = 0
+            for idxChirp in range(self.idxProcChirp//2 - chirpPad//2, self.idxProcChirp//2 + chirpPad//2):
+                dataFFTGroup[i, :, :] = self.postProcessFFT(np.transpose(dataTemp[idxChirp, :, :]))
+                i += 1
+            return dataFFTGroup
+        else:       
+            dataTemp = np.zeros((self.idxProcChirp, self.numADCSamples//self.adcRatio, self.numAngleBins), dtype='complex_')
+            for idxRX in range(self.numAngleBins):
+                for idxADC in range(self.numADCSamples//self.adcRatio):
+                    #dataTemp[:, idxADC, idxRX] = dataRangeAzimuth[idxRX, :, idxADC]
+                    #dataTemp[:, idxADC, idxRX] = np.fft.fftshift(dataTemp[:, idxADC, idxRX], axes=(0))
+                    dataTemp[:, idxADC, idxRX] = dataRangeAzimuth[idxRX, :, idxADCSpecific[idxADC]]
+                    dataTemp[:, idxADC, idxRX] = np.fft.fftshift(dataTemp[:, idxADC, idxRX], axes=(0))
+            dataFFT = self.postProcessFFT(np.transpose(np.sum(dataTemp, axis=0)))
             return dataFFT
-
-    def generateRangeDopplerMap(self, frame):
-        dataRangeDoppler = np.zeros((self.numRX*2, self.numChirp//2, self.numADCSamples), dtype='complex_')
-        for idxRX in range(self.numRX):
-            for idxChirp in range(self.numChirp//2):
-                dataRangeDoppler[idxRX, idxChirp] = (frame[idxRX, idxChirp*2] + frame[idxRX, idxChirp*2+1])/2
-                dataRangeDoppler[idxRX+4, idxChirp] = (frame[idxRX, idxChirp*2] - frame[idxRX, idxChirp*2+1])/2
-        for idxRX in range(self.numRX*2):
-            dataRangeDoppler[idxRX, :, :] = np.fft.fft2(dataRangeDoppler[idxRX, :, :])
-        #dataFFT = dataRangeDoppler[:,:,0:self.numADCSamples//2].max(axis=0)
-        dataFFT = dataRangeDoppler[0,:,0:self.numADCSamples//2]
-        dataFFT = postProcessFFT(dataFFT, rescale=True)
-        return dataFFT
 
     def saveDataAsFigure(self, img, joints, 
         mapRangeAzimuthHori, mapRangeAzimuthVert, 
         visDirName, idxFrame):
-        pose = None
+        pose = img
         heatmap = None
         if self.showPose:
             pose = PlotPose(img, joints)
@@ -315,15 +329,6 @@ class RadarObject():
     def saveRadarData(self, matrix, dirName, idxFrame):
         dirSave = dirName + ('/%09d' % idxFrame) + '.npy'
         np.save(dirSave, matrix)
-        
-        #if self.saveWithChirp:
-        #    for idxChirp in range(self.idxProcChirp):
-        #        idxChirpFrame = (idxFrame * (self.idxProcChirp)) + idxChirp
-        #        dirSave = dirName + ('/%09d' % idxChirpFrame) + '.txt'
-        #        np.savetxt(dirSave, np.column_stack([matrix[idxChirp].real, matrix[idxChirp].imag]))
-        #else:
-        #    dirSave = dirName + ('/%09d' % idxFrame) + '.txt'
-        #    np.savetxt(dirSave, np.column_stack([matrix.real, matrix.imag]))
 
     def processRadarData(self):
         for idxName in range(len(self.radarDataFileNameGroup)):
@@ -342,16 +347,19 @@ class RadarObject():
             for idxFrame in range(0,self.numFrame):
                 frameHori = adcDataHori[:, self.numChirp*(idxFrame):self.numChirp*(idxFrame+1), 0:self.numADCSamples]
                 frameVert = adcDataVert[:, self.numChirp*(idxFrame):self.numChirp*(idxFrame+1), 0:self.numADCSamples]
-                mapRangeAzimuthHori = self.generateRangeAzimuthMap(frameHori)
-                mapRangeAzimuthVert = self.generateRangeAzimuthMap(frameVert)
+                
+                if self.useVelocity:
+                    mapRangeAzimuthHori = self.generateRangeDopplerAzimuthMap(frameHori)
+                    mapRangeAzimuthVert = self.generateRangeDopplerAzimuthMap(frameVert)
+                else:
+                    mapRangeAzimuthHori = self.generateRangeAzimuthMap(frameHori)
+                    mapRangeAzimuthVert = self.generateRangeAzimuthMap(frameVert)
+                
                 if self.visualization:
-                    if self.saveWithChirp:
-                        mapRangeAzimuthHori = mapRangeAzimuthHori[0,:,:]#only take the first chirp
-                        mapRangeAzimuthVert = mapRangeAzimuthVert[0,:,:]
                     visDirName = self.saveDirNameGroup[idxName] + '/visualization' + ('/%09d.png' % idxFrame)
-                    
                     if self.showPose or self.showHeatmap:
                         img = np.array(Image.open(self.rgbFileNameGroup[idxName] + "/%09d.jpg" % idxFrame).convert('RGB'))
+                        #img = None
                         joints = annotGroup[idxFrame]['joints']
                     else:
                         img = None
@@ -360,12 +368,11 @@ class RadarObject():
                         mapRangeAzimuthHori, mapRangeAzimuthVert, 
                         visDirName, idxFrame)
                 else:
-                    horiDirName = self.saveDirNameGroup[idxName] + '/hori' #+ ('/%09d' % idxFrame) + '.txt'
-                    self.saveRadarData(mapRangeAzimuthHori, horiDirName, idxFrame)
-                    vertDirName = self.saveDirNameGroup[idxName] + '/verti' #+ ('/%09d' % idxFrame) + '.txt'
-                    self.saveRadarData(mapRangeAzimuthVert, vertDirName, idxFrame)
+                    self.saveRadarData(mapRangeAzimuthHori, self.saveDirNameGroup[idxName] + '/hori', idxFrame)
+                    self.saveRadarData(mapRangeAzimuthVert, self.saveDirNameGroup[idxName] + '/verti', idxFrame)
 
                 print('%s, finished frame %d' % (self.radarDataFileNameGroup[idxName][0], idxFrame), end='\r')
+                print(mapRangeAzimuthHori.shape, end='\r')
 if __name__ == "__main__":
-    radarObject = RadarObject(visualization=False, saveWithChirp=True)
+    radarObject = RadarObject(visualization=False)
     radarObject.processRadarData()
--- process_videos.py
+++ process_videos.py
@@ -13,11 +13,11 @@ def GenerateFramesfromVideo(video_path, save_path, isFliped=False):
             image = cv2.flip(image, 0)
         cv2.imwrite(save_path+"/%09d.jpg" % count, image)     # save frame as JPEG file      
         success, image = vidcap.read()
-        print('%s, save a new frame: %d' % (video_path, count))
+        print('%s, save a new frame: %d' % (video_path, count), end="\r")
         count += 1
     return count
 
-def PreprcessFrames(num_frames, load_path, save_path):
+def PreprocessFrames(num_frames, load_path, save_path):
     transform_fn = transforms.Compose([transforms.ToTensor()])
     toimg = transforms.ToPILImage()
     for i in range(num_frames):
@@ -30,105 +30,281 @@ def PreprcessFrames(num_frames, load_path, save_path):
         print('Save a new frame: %d' % i)
         #img.show()
 
-def PreprcessSpecificFrames(load_path, save_path, p_frame_num):
+def PreprocessSpecificFrames(load_path, save_path, p_frame_num):
+    #transform_fn = transforms.Compose([transforms.CenterCrop(512), transforms.ToTensor()])
     transform_fn = transforms.Compose([transforms.ToTensor()])
     toimg = transforms.ToPILImage()
     i = 0
     for idx in range(p_frame_num, p_frame_num+1800):
         img = Image.open(load_path+"/%09d.jpg" % idx).convert('RGB')
         img = transform_fn(img)
+        #img = toimg(img)
         #print(img.size())
         img = F.interpolate(img.unsqueeze(0), size=[512, 512]) # 256, 256
         img = toimg(img.squeeze(0))
         img.save(save_path+"/%09d.jpg" % i)
-        print('%s, save a old frame %d as a new frame %d' % (save_path, idx, i))
+        print('%s, save a old frame %d as a new frame %d' % (save_path, idx, i), end='\r')
         i += 1
+
+def GenerateFramesfromVideoProcessFrames(video_path, save_path, p_frame_num, isFliped=False):
+    vidcap = cv2.VideoCapture(video_path)
+    success, image = vidcap.read()
+    count = 0
+    idx = 0
+    while success:
+        if count >= p_frame_num and count < (p_frame_num+1800):
+            if isFliped:
+                image = cv2.flip(image, 0)
+                #image = cv2.flip(image, -1)
+            image = cv2.resize(image, (512, 512), interpolation = cv2.INTER_LINEAR)
+            cv2.imwrite(save_path+"/%09d.jpg" % idx, image)     # save frame as JPEG file 
+            print('%s, save a new frame: %d' % (video_path, count), end="\r")
+            idx += 1
+        success, image = vidcap.read()
+        count += 1
+    return count
 
 def GenerateVideofromFrames(frame_path, save_path):
     #img = cv2.imread(frame_path+'/RAmap0.png')
+    #print(frame_path)
     img = cv2.imread(frame_path + ('/%09d.png' % 0))
     fps = 30
     size = (img.shape[1],img.shape[0])
-    print(size)
+    #print(size)
     fourcc = cv2.VideoWriter_fourcc(*"XVID")
     videoWrite = cv2.VideoWriter(save_path, fourcc,fps,size)
 
-
+    
     for i in range(0,1800):
         #fileName = frame_path+'/RAmap%d.png'%i
         fileName = frame_path+'/%09d.png' % i
         img = cv2.imread(fileName)
         videoWrite.write(img)
-        print(i)
-
-#for 20210609
-#startFrameGroup = [
-#        139,
-#        75,
-#        63,
-#        147,
-#        103,
-#        247
-#    ]
-
-# fpr 20210628
-# startFrameGroup = [
-#         156,
-#         96,
-#         22,
-#         167,
-#         275,
-#         12,
-#         21,
-#         8,
-#         17,
-#         16,
-#         423,
-#         192,
-#         207,
-#         182,
-#         233,
-#         205,
-#         386,
-#         231,
-#         171,
-#         158,
-#         6,
-#         8,# should be 14, but total length is not enough
-#         6,
-#         8,
-#         0,
-#         8,
-#         3,
-#         11,
-#         7,
-#         12
-#     ]
+        print(i, end='\r')
 
 
-#for 20210628
+
+
+#for videos collected after 20210628
 #the video clips should be flipped (isFliped=True)
 
 if __name__ == "__main__":
     
-    nameGroup = ['single_%d' % i for i in range(1,18)]
-    root = '20210712'
+    nameGroup = ['single_%d' % i for i in range(1, 1+1)]
+    #nameGroup = ['single_%d' % i for i in range(1, 15+1)]
+    root = '20211220'
     
-    for idxName in range(len(nameGroup)):
-        name = nameGroup[idxName]
-        pathVideo = "videos/" + root + "/" + name + ".mp4" # ".avi"
-        pathRawFrame = "frames/" + root + "/" + name + "/raw"
-        numFrames = GenerateFramesfromVideo(pathVideo, pathRawFrame, True)
-        #PreprcessFrames(numFrames, pathRawFrame, pathProcessedFrame)
+    # for idxName in range(len(nameGroup)):
+    #    name = nameGroup[idxName]
+    #    pathVideo = "videos/" + root + "/" + name + ".mp4" # ".avi"
+    #    pathRawFrame = "frames/" + root + "/" + name + "/raw"
+    #    numFrames = GenerateFramesfromVideo(pathVideo, pathRawFrame, True)
+    #    #PreprocessFrames(numFrames, pathRawFrame, pathProcessedFrame)
 
     # startFrameGroup = [
+    #     166, 
+    #     91, 
+    #     123, 
+    #     102, 
+    #     103, 
+    #     84, 
+    #     80, 
+    #     96, 
+    #     83, 
+    #     76, 
+    #     87, 
+    #     83, 
+    #     76, 
+    #     74, 
+    #     83
     # ]
+
+    #0916
+    # startFrameGroup = [
+    #     101, 
+    #     101, 
+    #     89, 
+    #     111, 
+    #     112, 
+    #     248, 
+    #     121, 
+    #     136, 
+    #     79, 
+    #     84, 
+    #     91, 
+    #     80, 
+    #     123, 
+    #     99, 
+    #     87
+    # ]
+    # ''' 20211106_long_range '''
+    # startFrameGroup = [
+    #     68, 
+    #     99, 
+    #     78, 
+    #     81, 
+    #     79, 
+    #     69, 
+    #     60, 
+    #     66, 
+    #     100, 
+    #     54, 
+    #     51, 
+    #     68, 
+    #     72, 
+    #     50, 
+    #     52, 
+    #     63, 
+    #     47, 
+    #     59, 
+    #     60, 
+    #     62, 
+    #     89, 
+    #     79, 
+    #     122, 
+    #     82, 
+    #     160, 
+    #     72, 
+    #     89, 
+    #     92, 
+    #     65, 
+    #     78, 
+    #     71, 
+    #     70, 
+    #     76, 
+    #     96, 
+    #     78, 
+    #     88, 
+    #     94, 
+    #     56, 
+    #     71, 
+    #     89, 
+    #     51, 
+    #     54, 
+    #     46, 
+    #     66, 
+    #     55, 
+    #     70, 
+    #     50, 
+    #     68, 
+    #     54, 
+    #     48, 
+    #     61, 
+    #     64, 
+    #     55, 
+    #     57, 
+    #     55, 
+    #     62, 
+    #     66, 
+    #     51, 
+    #     66, 
+    #     56, 
+    #     55, 
+    #     54, 
+    #     65, 
+    #     56, 
+    #     60, 
+    #     54, 
+    #     59, 
+    #     74, 
+    #     56, 
+    #     56, 
+    #     61, 
+    #     56, 
+    #     60, 
+    #     64, 
+    #     71, 
+    #     61, 
+    #     56, 
+    #     53, 
+    #     46, 
+    #     57, 
+    #     64, 
+    #     76, 
+    #     116, 
+    #     62, 
+    #     45, 
+    #     89, 
+    #     88, 
+    #     61, 
+    #     73, 
+    #     43, 
+    #     61, 
+    #     78, 
+    #     67, 
+    #     54, 
+    #     34, 
+    #     46, 
+    #     77, 
+    #     80, 
+    #     35, 
+    #     85, 
+    #     58, 
+    #     61, 
+    #     55, 
+    #     64, 
+    #     53, 
+    #     55, 
+    #     56, 
+    #     56, 
+    #     61, 
+    #     60, 
+    #     80, 
+    #     56, 
+    #     65, 
+    #     62, 
+    #     57, 
+    #     54, 
+    #     49, 
+    #     61, 
+    #     61, 
+    #     53
+    # ]
+
+    # for idxName in range(len(nameGroup)):
+    #    name = nameGroup[idxName]
+    #    pathRawFrame = "frames/" + root + "/" + name + "/raw"
+    #    pathProcessedFrame = "frames/" + root + "/" + name + "/processed/images"
+    #    PreprocessSpecificFrames(pathRawFrame, pathProcessedFrame, startFrameGroup[idxName])
+    
+    # 20211220_ShortRangeConfig
+    startFrameGroup = [
+        89, 114, 101, 94, 80, 
+        94, 81, 94, 86, 84, 
+        52, 56, 75, 92, 70, 
+        55, 62, 69, 86, 63, 
+        52, 53, 66, 52, 67, 
+        64, 99, 61, 67, 64
+    ]
+    # 20220107_LongRangeConfig
+    # startFrameGroup = [
+	# 	121, 96, 70, 135, 76, 
+	# 	81, 95, 136, 71, 86, 
+	# 	95, 156, 162, 118, 117, 
+	# 	117, 140, 108, 98, 100, 
+	# 	83, 130, 129, 120, 88, 
+	# 	108, 124, 96, 168, 96
+    # ]
+
+    #for specific
+    # nameGroup = ['single_1', 'single_7', 'single_13', 'single_19', 'single_25']
+    # nameIdx = [1, 7, 13, 19, 25]
+    # for idxName in range(len(nameIdx)):
+    #     name = nameGroup[idxName]
+    #     pathVideo = "videos/" + root + "/" + name + ".mp4" # ".avi"
+    #     pathProcessedFrame = "frames/" + root + "/" + name + "/processed/images"
+    #     GenerateFramesfromVideoProcessFrames(pathVideo, pathProcessedFrame, startFrameGroup[nameIdx[idxName]-1], True)
+    
+    #for all
     # for idxName in range(len(nameGroup)):
     #     name = nameGroup[idxName]
-    #     pathRawFrame = "frames/" + root + "/" + name + "/raw"
+    #     pathVideo = "videos/" + root + "/" + name + ".mp4" # ".avi"
     #     pathProcessedFrame = "frames/" + root + "/" + name + "/processed/images"
-    #     PreprcessSpecificFrames(pathRawFrame, pathProcessedFrame, startFrameGroup[idxName])
-    
-    # pathVideoFrame = "/media/pc3426/B2E67301E672C4DF/radar_skeleton_estimation/radar_skeleton_estimation_network/visualization/for20210706_presentation/multiFramesChirps_tmv2_20210707_val_2"
-    # pathVideo = "./videos/20210628/multiFramesChirps_tmv2_20210707_val_2.avi"
-    # GenerateVideofromFrames(pathVideoFrame, pathVideo)
+    #     GenerateFramesfromVideoProcessFrames(pathVideo, pathProcessedFrame, startFrameGroup[idxName], True)
+
+
+    #pathVideoFrame = "/work/robert80203/Radar_detection/radar_skeleton_estimation/data/20211220_Pad_CltRm_RDA/single_6/visualization"
+    pathVideoFrame = "radar_skeleton_estimation/visualization/20220107/eccv2022/stunet_df/single_19"
+    #pathVideo = "./videos/20211220/gt/single_25.avi"
+    pathVideo = "./videos/20220107/eccv2022/stunet_df_single_19.avi"
+    GenerateVideofromFrames(pathVideoFrame, pathVideo)
